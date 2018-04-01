5883fa821ba8e755efc5d12fe558026b0a6815e7
AntonOcean <warres@yandex.ru>
2018-04-01 13:33:44 +0300

    token

--- contracts/SmartToken.sol
+++ contracts/SmartToken.sol
@@ -1,13 +1,13 @@
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.19; // версия компилятора
 
-
+// откуда берем стандартные токены
 import 'zeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
 import 'zeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
 import 'zeppelin-solidity/contracts/token/ERC20/BurnableToken.sol';
 
 
-contract SmartToken is StandardToken, MintableToken, BurnableToken {
-    string public name = 'Antoxa Token';
-    string public symbol = 'ANT';
-    uint public decimals = 18;
+contract SmartToken is StandardToken, MintableToken, BurnableToken { // наследование от стандартных токенов
+    string public name = 'Antoxa Token'; // имя токена
+    string public symbol = 'ANT'; // символ токена
+    uint public decimals = 18; // количество знаков после запятой, вей
 }
--- test/AntoxaToken.js
+++ test/AntoxaToken.js
@@ -1,12 +1,28 @@
-'use strict'
+'use strict';
 
-const Token = artifacts.require('SmartToken.sol');
+const Token = artifacts.require('SmartToken.sol'); // контракт
 
-contract('SmartToken', async (accounts) => {
+contract('SmartToken', async (accounts) => { // массив аккаунтов из тестовой ноды
     it('simple test', async () => {
         let instance = await Token.new({from: accounts[0]});
-        await instance.mint(accounts[1], 100, {from: accounts[0]});
-        assert.equal(100, await instance.balanceOf(accounts[1]));
+        // деплой(от 0ого акка) контракта в тестовую сеть(локальная)
 
+        await instance.mint(accounts[1], 100, {from: accounts[0]});
+        // создание токенов для адреса с помощью mint из MintableToken
+
+        assert.equal(100, await instance.balanceOf(accounts[1]));
+        // проверка баланса по адресу, balanceOf из Basic token
+
+        await instance.transfer(accounts[2], 50, {from: accounts[1]});
+        // перевод 50 вей от 1 ого аккаунта ко 2 аккаунту
+        assert.equal(50, await instance.balanceOf(accounts[1]));
+        assert.equal(50, await instance.balanceOf(accounts[2]));
+
+        await instance.burn(50, {from: accounts[2]});
+        // сжигаем 50 вей
+        assert.equal(0, await instance.balanceOf(accounts[2]));
+
+        assert.equal(50, await instance.totalSupply());
+        // Basic token
     })
-});
+}); // тест для контракта
